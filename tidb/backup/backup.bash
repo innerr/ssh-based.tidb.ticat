a1dd4d29aa1cde9cd56a58c1ebdb90efb1b12f68
Liu Cong <innerr@gmail.com>
2021-06-23 16:49:15 +0800

    allow skip existed backup
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- tidb/backup/backup.bash
+++ tidb/backup/backup.bash
@@ -6,6 +6,15 @@ here=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
 . "${here}/base.bash" "${env}" 'true'
 
 tag=`env_val "${env}" 'tidb.backup.tag'`
+skip=`env_val "${env}" 'tidb.backup.skip-exist'`
+
+trues=('true' 't' 'yes' 'y' 'on' '1')
+for t in ${trues[@]}; do
+	if [ "${t}" == "${skip}" ]; then
+		skip='true'
+		break
+	fi
+done
 
 for (( i = 0; i < ${cnt}; ++i)) do
 	host="${hosts[$i]}"
@@ -16,8 +25,13 @@ for (( i = 0; i < ${cnt}; ++i)) do
 	exists=`ssh_exe "${host}" "test -d \"${dir}.${tag}\" && echo exists"`
 	set -e
 	if [ ! -z "${exists}" ]; then
-		echo "[:(] '${host}:${dir}.${tag}' exists, backup failed"
-		exit 1
+		if [ "${skip}" != 'true' ]; then
+			echo "[:(] '${host}:${dir}.${tag}' exists, backup failed"
+			exit 1
+		else
+			echo "[:-] '${host}:${dir}.${tag}' exists, skipped"
+			exit 0
+		fi
 	fi
 
 	cmd="rm -rf \"${dir}.${tag}\" && rm -f \"${dir}/data/space_placeholder_file\" && cp -rp \"${dir}\" \"${dir}.${tag}\""
