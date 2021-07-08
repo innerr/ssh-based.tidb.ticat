fddb652095e1c3d2cd115ff638dad839f9eab003
Liu Cong <innerr@gmail.com>
2021-07-08 13:22:54 +0800

    better args of backup
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- tidb/backup/backup.bash
+++ tidb/backup/backup.bash
@@ -4,15 +4,16 @@ set -euo pipefail
 env=`cat "${1}/env"`
 shift
 
-shift 3
-use_mv=`to_true "${1}"`
-
 # export: $pri_key, $user, $cnt, $hosts, $dirs
 get_instance_info "${env}" 'true'
 
 tag=`must_env_val "${env}" 'tidb.backup.tag'`
-skip=`must_env_val "${env}" 'tidb.backup.skip-exist'`
-skip=`to_true "${skip}"`
+
+skip_exist=`must_env_val "${env}" 'tidb.backup.skip-exist'`
+skip_exist=`to_true "${skip_exist}"`
+
+use_mv=`must_env_val "${env}" 'tidb.backup.use-mv'`
+use_mv=`to_true "${use_mv}"`
 
 for (( i = 0; i < ${cnt}; ++i)) do
 	host="${hosts[$i]}"
@@ -23,7 +24,7 @@ for (( i = 0; i < ${cnt}; ++i)) do
 	exists=`ssh_exe "${host}" "test -d \"${dir}.${tag}\" && echo exists"`
 	set -e
 	if [ ! -z "${exists}" ]; then
-		if [ "${skip}" != 'true' ]; then
+		if [ "${skip_exist}" != 'true' ]; then
 			echo "[:(] '${host}:${dir}.${tag}' exists, backup failed"
 			exit 1
 		else
