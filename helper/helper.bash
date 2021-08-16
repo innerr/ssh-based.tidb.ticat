52088a124b0a3b8f5fb8dd7f35ecb1cc8d864530
Liu Cong <innerr@gmail.com>
2021-08-16 17:50:29 +0800

    fix import name in helper
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- helper/helper.bash
+++ helper/helper.bash
@@ -1,2 +1,2 @@
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/ticat.helper.bash/helper.bash"
-. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/tiup.helper.bash/helper.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/tiup.helper.bash/tiup.bash"
